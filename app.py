76dd509f592da625bf2516cfbf948b2a03ab8692
Eny02 <105815202+Eny02@users.noreply.github.com>
2024-02-07 02:00:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'Server=tcp:zad5python.database.windows.net,1433;Initial Catalog=Zadanie5python;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
@@ -17,13 +9,8 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
-    email = request.form.get('email')
-    
-    new_user = User(name=name, email=email)
-    db.session.add(new_user)
-    db.session.commit()
-    
-    return 'Dane przesłane i zapisane do bazy danych!'
+    age = request.form.get('age')
+    return f'Dane przesłane: Imię - {name}, Wiek - {age}'
 
 if __name__ == '__main__':
     app.run(debug=True)
