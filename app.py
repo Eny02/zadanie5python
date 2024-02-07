c48a8be33748f0d803b36ef2cbef135de5597d7c
Eny02 <105815202+Eny02@users.noreply.github.com>
2024-02-07 10:23:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
-    age = request.form.get('email')
+    email = request.form.get('email')
     return f'Dane przesłane: Imię - {name}, E-Mail - {email}'
 
 if __name__ == '__main__':
